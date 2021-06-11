215f2e5869898f31738c63fcc2d83d2f09be2627
Alexis Medina <alexisnpavlik@gmail.com>
2021-06-11 13:45:56 -0300

    s

--- Ejercitación/Recolectador de polillas.py
+++ Ejercitación/Recolectador de polillas.py
@@ -1,3 +1,9 @@
+# Se inicia una campaña de recolección de colillas de cigarrillos en los barrios.
+
+# Realizá un programa que permita registrar cantidad de colillas recolectadas por un número
+# determinado de personas. Luego obtener estadísticas al respecto informando porcentaje de personas
+# que han encontrado menos de 100 colillas, más de 100 y menos de 200, más de 200 colillas.
+
 colillas = int(input("Ingrese la cantidad de colillas que ha recolectado:   "))
 persona = 1
 per_mas100=0
